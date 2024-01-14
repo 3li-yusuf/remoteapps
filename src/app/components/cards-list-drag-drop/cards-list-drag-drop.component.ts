54ad0a51f7e917bebccd028faf9c264abc3dea54
3li-yusuf <141238830+3li-yusuf@users.noreply.github.com>
2024-01-14 13:19:52 +0300

    final

--- src/app/components/cards-list-drag-drop/cards-list-drag-drop.component.ts
+++ src/app/components/cards-list-drag-drop/cards-list-drag-drop.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, NgModule, OnInit } from '@angular/core';
-
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+} from '@angular/core';
 import {
   CdkDragDrop,
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cards-list-drag-drop',
   templateUrl: './cards-list-drag-drop.component.html',
   styleUrls: ['./cards-list-drag-drop.component.scss'],
 })
-export class CardsListDragDropComponent implements OnInit {
+export class CardsListDragDropComponent implements OnInit, AfterViewInit {
+  @ViewChild('modal') modal;
   @Input() listName: string;
   @Input() listData: Array<{
     pic: string;
@@ -20,13 +29,30 @@ export class CardsListDragDropComponent implements OnInit {
     role: string;
   }>;
   @Input() listColor: string;
+  isModalOpen = false;
+
   constructor() {}
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.modal.onDidDismiss().then(() => {
+      this.isModalOpen = false;
+    });
+  }
+
   onLongPress(event) {
     console.log('Long Pressed!', event);
+    this.isModalOpen = true;
+    this.modal.present();
   }
+
+  closeModal() {
+    if (this.isModalOpen) {
+      this.modal.dismiss();
+    }
+  }
+
   drop(
     event: CdkDragDrop<
       { pic: string; name: string; nationality: string; role: string }[]
